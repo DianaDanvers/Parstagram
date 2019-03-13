74c6a30c27f678cd9f77a5b97505dc324225b5aa
Diana Danvers <itranscode@Dianas-MacBook-Pro.local>
2019-03-13 11:10:07 -0700

    Final II

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Diana Danvers on 3/4/19.
+//  Copyright © 2019 Diana Danvers. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
