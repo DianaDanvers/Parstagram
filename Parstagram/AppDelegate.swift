f2021518be39d7d70dad73cf0b5895ac92807225
Diana Danvers <itranscode@Dianas-MacBook-Pro.local>
2019-03-13 11:24:13 -0700

    Fix?

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -22,6 +22,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://hidden-eyrie-86874.herokuapp.com/parse"
             })
         )
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+        }
+        
         return true
     }
 
